3a3375f50822d3f8db7720e68bed8503afa70399
Kelvin Lin <kelvin@bu.edu>
2023-03-23 17:06:01 +0000

    added condA.s

---
+++ condA.s
@@ -0,0 +1,35 @@
+	.intel_syntax noprefix
+
+	.section .data
+data0:	 .quad 0xdead
+data1:	 .quad 0xbeef
+data2:	 .quad 0xfeed
+data3:	 .quad 0xface
+
+	.section .text
+	.global _start
+
+_start:
+	xor, rcx, rcx
+	and rax, 0xf
+	mov rbx, QWORD PTR[data0]
+	and rbf, 0xf
+	cmp rax, rbx
+	jne A
+	inc rcx
+A:	 mov rbx, QWORD PTR[data1]
+	and rbx, 0xf
+	cmp rax, rbx
+	jne B
+	inc rcx
+B:	mov rbx, QWPRD PTR[data2]
+	and rbx, 0xf
+	cmp rax, rbx
+	jne C
+	inc rcx
+C:	mov rbx, QWORD PTR[data3]
+	and rbx, 0xf
+	test rax, rbx
+	jne D
+	inc rcx
+D:	int3
